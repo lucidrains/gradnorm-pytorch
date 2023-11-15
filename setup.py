770013364e2d8177f2a77b7caf9223b247f58506
lucidrains <lucidrains@gmail.com>
2023-11-15 10:29:53 -0800

    scaffold

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'gradnorm-pytorch',
+  packages = find_packages(exclude=[]),
+  version = '0.0.1',
+  license='MIT',
+  description = 'GradNorm - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/gradnorm-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'loss balancing',
+    'gradient normalization'
+  ],
+  install_requires=[
+    'einops>=0.7.0',
+    'torch>=2.0'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
