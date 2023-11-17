d89dc183e17a313db7269fb89181cc90650262b9
lucidrains <lucidrains@gmail.com>
2023-11-17 09:27:18 -0800

    chipping away at the problem

--- gradnorm_pytorch/gradnorm_pytorch.py
+++ gradnorm_pytorch/gradnorm_pytorch.py
@@ -2,7 +2,7 @@ import torch
 import torch.distributed as dist
 from torch.autograd import grad
 from torch import nn, einsum, Tensor
-from torch.nn import Module, ModuleList
+from torch.nn import Module, ModuleList, Parameter
 
 from einops import rearrange
 
@@ -16,9 +16,12 @@ from beartype.typing import Optional, Union, List
 def exists(v):
     return v is not None
 
+def default(v, d):
+    return v if exists(v) else d
+
 # main class
 
-class GradNorm(Module):
+class GradNormLossWeighting(Module):
     @beartype
     def __init__(
         self,
@@ -27,6 +30,7 @@ class GradNorm(Module):
             Tensor
         ],
         *,
+        grad_norm_layer: Optional[Parameter] = None,
         accelerator: Optional[Accelerator] = None,
         frozen = False
     ):
@@ -38,13 +42,29 @@ class GradNorm(Module):
         assert loss_weights.ndim == 1, 'loss weights must be 1 dimensional'
         assert frozen, 'only frozen implemented'
 
+        num_losses = loss_weights.numel()
+
         self.accelerator = accelerator
-        self.num_losses = loss_weights.numel()
+        self.num_losses = num_losses
         self.frozen = frozen
 
-        self.loss_weights = nn.Parameter(loss_weights, requires_grad = not frozen)
+        self._grad_norm_layer = [grad_norm_layer] # hack
+
+        # loss weights, either learned or static
+
+        self.loss_weights = Parameter(loss_weights, requires_grad = not frozen)
+
+        # todo: figure out how best to smooth L0 over course of training, in case initialization of network is not great
+
+        self.register_buffer('initial_loss', torch.zeros(num_losses))
+
+        # step, for maybe having schedules etc
 
         self.register_buffer('step', torch.tensor(0.))
+
+    @property
+    def grad_norm_layer(self):
+        return self._grad_norm_layer[0]
 
     def backward(self, *args, **kwargs):
         return self.forward(*args, **kwargs)
@@ -55,7 +75,9 @@ class GradNorm(Module):
         losses: Union[
             List[Tensor],
             Tensor
-        ]
+        ],
+        shared_tensor: Optional[Tensor] = None,     # in the paper, they used the grad norm of penultimate parameters from a backbone layer. but this could also be activations (say shared image being fed to multiple discriminators)
+        freeze = False                              # can additionally freeze a learnable network on forward
     ):
         # backward functions dependent on whether using hf accelerate or not
 
@@ -74,9 +96,19 @@ class GradNorm(Module):
 
         # handle base frozen case, so one can freeze the weights after a certain number of steps, or just to a/b test against learned gradnorm loss weights
 
-        if self.frozen:
-            total_weighted_loss = (losses * self.loss_weights).sum()
+        if self.frozen or freeze:
+            total_weighted_loss = (losses * self.loss_weights.detach()).sum()
             backward(total_weighted_loss)
             return total_weighted_loss
+
+        # determine which tensor to get grad norm from
+
+        grad_norm_tensor = default(shared_tensor, self.grad_norm_layer)
+
+        assert grad_norm_tensor.requires_grad, 'requires grad must be turned on for the tensor for deriving grad norm'
+
+        # increment step
+
+        self.step.add_(1)
 
         raise NotImplementedError
